74915791aa5cfc4714e4fc925d0902e16c005019
Himanshu Ahire <himorithm@gmail.com>
2019-01-13 21:09:24 +0530

    Removed unwanted files

--- src/test/scala/com/himanshu/demo/springdatacassandra/controller/IntTestSpec.scala
+++
@@ -1,8 +0,0 @@
-package com.himanshu.demo.springdatacassandra.controller
-
-import org.scalatest.{FlatSpec, GivenWhenThen}
-import org.springframework.test.context.TestContextManager
-
-class IntTestSpec extends FlatSpec with GivenWhenThen{
-  new TestContextManager(this.getClass).prepareTestInstance(this)
-}
